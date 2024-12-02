61f456a77c3d308f9be5c15898f1ab345d04d367
heladerialanieve <proyectolanieve@gmail.com>
2024-12-02 05:30:41 +0000

    forms,serializer, signal borrados

--- pedidos/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Cliente
-
-class ArticuloSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Cliente
-        fields="__all__"
