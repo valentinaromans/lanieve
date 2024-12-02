fb06a6064c51833599d783153c53d39ace9ae7fd
heladerialanieve <proyectolanieve@gmail.com>
2024-12-02 05:11:59 +0000

    cambio settings

--- lanieve/settings.py
+++ lanieve/settings.py
@@ -37,18 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework.authtoken',
     'usuarios',
     'inicio',
     'helados',
     'pedidos',
-    'rest_framework',
-    'corsheaders',
 ]
 
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
