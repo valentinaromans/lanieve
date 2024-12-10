4b2c1e97576ed8b7590ee650fc5a5db821f55d0f
valentinaromans <valentina.roman03@inacapmail.cl>
2024-12-09 23:20:22 -0300

    cambios proyecto

--- lanieve/settings.py
+++ lanieve/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
